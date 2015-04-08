78ba13722c5b805603f3cebdc7f9487dd4dca449
piotrz <pierozyc@poczta.onet.pl>
2015-04-08 15:27:33 +0200

    implementation. Refactoring Score of Player A to integer

--- src/main/java/tennis/TennisGame.java
+++ src/main/java/tennis/TennisGame.java
@@ -1,20 +1,29 @@
 package tennis;
 
+import java.util.HashMap;
+
 /**
  * Created by Piotr on 2015-04-08.
  */
 public class TennisGame {
 
-    private String playerAScore = "0";
+    private int playerAScore = 0;
     private String playerBScore = "0";
     private static final String value = "15";
+    private HashMap<Integer,String> pointMap = new HashMap<Integer,String>();
+
+    public TennisGame(){
+        pointMap.put(0,"0");
+        pointMap.put(1,"15");
+        pointMap.put(2,"30");
+    }
 
     public String getCurrentScore(){
-        return playerAScore + " " + playerBScore;
+        return pointMap.get(playerAScore) + " " + playerBScore;
     }
 
     public void playerAScores(){
-        playerAScore = value;
+        playerAScore += 1;
     }
 
     public void playerBScores(){
