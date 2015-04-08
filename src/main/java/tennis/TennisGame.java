ef09734fc2293a81cb69a4749e369424f566bbe1
piotrz <pierozyc@poczta.onet.pl>
2015-04-08 14:58:23 +0200

    next implementation

--- src/main/java/tennis/TennisGame.java
+++ src/main/java/tennis/TennisGame.java
@@ -5,11 +5,13 @@ package tennis;
  */
 public class TennisGame {
 
+    private String playerAScore = "0";
+
     public String getCurrentSocre(){
-        return "0 0";
+        return playerAScore+" 0";
     }
 
     public void playerAScores(){
-
+        playerAScore = "15";
     }
 }
