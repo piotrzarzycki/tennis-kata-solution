bd9984d4e3861a9d6e6821f63df85f391603698e
piotrz <pierozyc@poczta.onet.pl>
2015-04-08 17:00:22 +0200

    refactoring

--- src/main/java/tennis/TennisGame.java
+++ src/main/java/tennis/TennisGame.java
@@ -12,6 +12,9 @@ public class TennisGame {
     private int playerBScore = 0;
     private Map<Integer,String>  diffMap = new HashMap<Integer, String>();
     private Map<Integer, String> pointMap = new HashMap<Integer,String>();
+    private final static String delimiter = " ";
+    private final static String Advantage = "A";
+    private final static String Nothing = "-";
 
     public TennisGame(){
         pointMap.put(0, "0");
@@ -19,15 +22,15 @@ public class TennisGame {
         pointMap.put(2, "30");
         pointMap.put(3, "40");
         diffMap.put(0, "deuce");
-        diffMap.put(1, "A -");
-        diffMap.put(-1, "- A");
+        diffMap.put(1, Advantage + delimiter + Nothing);
+        diffMap.put(-1, Nothing + delimiter + Advantage);
     }
 
     public String getCurrentScore(){
         if (playerAScore >=3 && playerBScore >= 3){
             return diffMap.get(playerAScore - playerBScore);
         }
-        return pointMap.get(playerAScore) + " " + pointMap.get(playerBScore);
+        return pointMap.get(playerAScore) + delimiter + pointMap.get(playerBScore);
     }
 
     public void playerAScores(){
